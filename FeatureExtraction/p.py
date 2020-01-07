2058dc19472cab46151052d999d254f804531813
Kataria <vakul.kataria@siemens.com>
2020-01-07 20:04:33 +0530

    New update

--- FeatureExtraction/p.py
+++ FeatureExtraction/p.py
@@ -3,18 +3,16 @@ import json
 from datetime import datetime
 import pandas as pd
 import numpy as np
-from multiprocessing import Process
-import threading
+from multiprocessing import Process, Manager
 import os
+from itertools import repeat
 
 dir = os.path.dirname(__file__)
 filename = os.path.join(dir, 'train.csv')
 
 dataFrameTrain = pd.read_csv(filename)
 
-lock = threading.Lock()
-
-def FindIdInColumn(column,callBack,fieldName):
+def FindIdInColumn(column,fieldName,returnList):
     for i in range(0,len(column)):
         collectionJson = column[i]
         if type(collectionJson) !=str or collectionJson == '':
@@ -28,57 +26,74 @@ def FindIdInColumn(column,callBack,fieldName):
                 if not(collectionJson[j].isspace()) and collectionJson[j].isnumeric():
                     idStr = idStr + collectionJson[j]
                 j=j+1
-            callBack(i,idStr)
+            returnList.append((i,idStr))
             idIndex = idIndex+2
             idIndex = collectionJson.find(fieldName,idIndex,len(collectionJson))
-
+    return returnList
 
 def CreateOrAddValueToCol(i,colName):
-    lock.acquire()
     dataFrameTrain.at[i,colName] = 1
-    lock.release()
 
 def AddValueToCollectionColumn(i,value):
-    lock.acquire()
     dataFrameTrain.at[i,'belongs_to_collection'] = value
-    lock.release()
-
-
-
-
 
 if __name__ == '__main__': 
-	p1 = Process(target=FindIdInColumn,args=(dataFrameTrain['genres'],CreateOrAddValueToCol,'\'id\''))
-	p2 = Process(target=FindIdInColumn,args=(dataFrameTrain['production_companies'],CreateOrAddValueToCol,'\'id\''))
-	p3 = Process(target=FindIdInColumn,args=(dataFrameTrain['production_countries'],CreateOrAddValueToCol,'\'name\''))
-	p4 = Process(target=FindIdInColumn,args=(dataFrameTrain['Keywords'],CreateOrAddValueToCol,'\'id\''))
-	p5 = Process(target=FindIdInColumn,args=(dataFrameTrain['Keywords'],CreateOrAddValueToCol,'\'id\''))
-	p6 = Process(target=FindIdInColumn,args=(dataFrameTrain['cast'],CreateOrAddValueToCol,'\'id\''))
-	p7 = Process(target=FindIdInColumn,args=(dataFrameTrain['crew'],CreateOrAddValueToCol,'\'id\''))
-	p8 = Process(target=FindIdInColumn,args=(dataFrameTrain['belongs_to_collection'],AddValueToCollectionColumn,'\'id\''))
+	manager = Manager()
+	dictList = []
+	p1List = manager.list()
+	dictList.append(p1List)
+	p2List = manager.list()
+	dictList.append(p2List)
+	p3List = manager.list()
+	dictList.append(p3List)
+	p4List = manager.list()
+	dictList.append(p4List)
+	p5List = manager.list()
+	dictList.append(p5List)
+	p6List = manager.list()
+	dictList.append(p6List)
+	p7List = manager.list()
+	dictList.append(p7List)
+	p8List = manager.list()
+	processList = []
+	p1 = Process(target=FindIdInColumn,args=(dataFrameTrain['genres'],'\'id\'',p1List))
+	processList.append(p1)
+	p2 = Process(target=FindIdInColumn,args=(dataFrameTrain['production_companies'],'\'id\'',p2List))
+	processList.append(p2)
+	p3 = Process(target=FindIdInColumn,args=(dataFrameTrain['production_countries'],'\'name\'',p3List))
+	processList.append(p3)
+	p4 = Process(target=FindIdInColumn,args=(dataFrameTrain['Keywords'],'\'id\'',p4List))
+	processList.append(p4)
+	p5 = Process(target=FindIdInColumn,args=(dataFrameTrain['Keywords'],'\'id\'',p5List))
+	processList.append(p5)
+	p6 = Process(target=FindIdInColumn,args=(dataFrameTrain['cast'],'\'id\'',p6List))
+	processList.append(p6)
+	p7 = Process(target=FindIdInColumn,args=(dataFrameTrain['crew'],'\'id\'',p7List))
+	processList.append(p7)
+	p8 = Process(target=FindIdInColumn,args=(dataFrameTrain['belongs_to_collection'],'\'id\'',p8List))
+	processList.append(p8)
 	print('Process start')
-	p1.start()
-	p2.start()
-	p3.start()
-	p4.start()            
-
-	p1.join()
-	p2.join()
-	p3.join()
-	p4.join()
-	print('second batch start')
-	p5.start()
-	p6.start()
-	p7.start()
-	p8.start()
-
-	p5.join()
-	p6.join()
-	p7.join()
-	p8.join()
-
-
+	for process in processList:
+		process.start()
+	for process in processList:
+		process.join()
 	print('process joined')
+	dfRowCount = dataFrameTrain.shape[0]
+	d = {}
+	for pList in dictList:
+		for t in pList:
+			if t[1] in d.keys():
+				d[t[1]][t[0]] = 1
+			else:
+				l = list(repeat(0,dfRowCount))
+				l[t[0]] = 1
+				d[t[1]] = l
+	df1 = pd.DataFrame.from_dict(d)
+	dataFrameTrain = dataFrameTrain.join(df1,how='left')
+	print('checkpoint 1')
+	for t in p8List:
+		AddValueToCollectionColumn(t[0],t[1])
+	print('checkpoint 2')
 	budgetCollection = dataFrameTrain.budget
 	for i in range(0,len(budgetCollection)):
 		budget = budgetCollection[i]
@@ -96,4 +111,4 @@ if __name__ == '__main__':
 		dataFrameTrain.at[i,'day'] = date.day
 		dataFrameTrain.at[i,'month'] = date.month
 		dataFrameTrain.at[i,'year'] = date.year
-dataFrameTrain.to_excel('finalMovieDatabse.xlsx')
+	dataFrameTrain.to_excel('E:\\finalMovieDatabse.xlsx')
